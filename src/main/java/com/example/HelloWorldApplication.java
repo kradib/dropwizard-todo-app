39805784be32c794d2bc465ca0193b6e5ff776df
kradib <radibkar32@gmail.com>
2024-06-02 12:22:27 +0530

    boilerplate code

---
+++ src/main/java/com/example/HelloWorldApplication.java
@@ -0,0 +1,26 @@
+package com.example.dropwizardapp;
+
+import com.example.HelloWorldConfiguration;
+import com.example.resource.HelloWorldResource;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        // Initialization logic if needed
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration, Environment environment) {
+        // Application logic
+        final HelloWorldResource resource = new HelloWorldResource();
+        environment.jersey().register(resource);
+    }
+}
